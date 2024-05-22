80b649b33874bd753b0dafecf8956717684cb0ca
aaromarc327 <aaromarc327@SDS-L-763HC63>
2024-05-22 14:20:56 -0400

    Add project files.

---
+++ SpaceRace/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SpaceRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(151, 557);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(74, 23);
+            this.p1ScoreLabel.TabIndex = 0;
+            this.p1ScoreLabel.Text = "0";
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(376, 557);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(74, 23);
+            this.p2ScoreLabel.TabIndex = 1;
+            this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(101, 267);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(399, 25);
+            this.subtitleLabel.TabIndex = 3;
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(166, 233);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(269, 34);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ SpaceRace/Form1.cs
@@ -0,0 +1,288 @@
+﻿//Aaron Marchanton
+//May 22, 2024
+//Space Race
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SpaceRace
+{
+    public partial class Form1 : Form
+    {
+        //Create Objects and Brushes
+        Rectangle player1 = new Rectangle(182, 480, 10, 25);
+        Rectangle player2 = new Rectangle(406, 480, 10, 25);
+        Rectangle zoneborder1 = new Rectangle(0, 530, 600, 3);
+        Rectangle zoneborder2 = new Rectangle(0, 455, 600, 3);
+
+        SolidBrush p1Brush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush p2Brush = new SolidBrush(Color.Goldenrod);
+        SolidBrush grayBrush = new SolidBrush(Color.LightGray);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        //New Randoms and Integers
+        Random randGen = new Random();
+        int randValue = 0;
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed = 4;
+        int meteorSize = 6;
+
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
+
+        //List of balls
+        List<Rectangle> meteorList = new List<Rectangle>();
+        List<int> meteorSpeeds = new List<int>();
+
+        //Sounds Create
+        SoundPlayer PointSound = new SoundPlayer(Properties.Resources.PointSound);
+        SoundPlayer WinSound = new SoundPlayer(Properties.Resources.WinSound);
+        SoundPlayer ExplosionSound = new SoundPlayer(Properties.Resources.ExplosionSound);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Keys Input (Unpress)
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.Space:
+                    gameTimer.Enabled = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Keys Input (Press)
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.Space:
+                    if (gameTimer.Enabled == false)
+                    {
+                        ResetGame();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameTimer.Enabled == false)
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Complete Actions
+            PlayerMovement();
+            MeteorsSpawn();
+            MeteorHitsPlayer();
+            ScoresOrWins();
+
+            Refresh();
+        }
+
+        public void PlayerMovement()
+        {
+            //move player 1
+            if (wPressed == true && player1.Y > 0)
+            {
+                player1.Y = player1.Y - playerSpeed;
+            }
+            if (sPressed == true && player1.Y < 505)
+            {
+                player1.Y = player1.Y + playerSpeed;
+            }
+
+            //move player 2
+            if (upPressed == true && player2.Y > 0)
+            {
+                player2.Y = player2.Y - playerSpeed;
+            }
+            if (downPressed == true && player2.Y < 505)
+            {
+                player2.Y = player2.Y + playerSpeed;
+            }
+        }
+
+        public void MeteorsSpawn()
+        {
+            //create balls
+            for (int i = 0; i < meteorList.Count(); i++)
+            {
+                int x = meteorList[i].X + meteorSpeeds[i];
+
+                meteorList[i] = new Rectangle(x, meteorList[i].Y, meteorSize, meteorSize);
+            }
+
+            randValue = randGen.Next(0, 100);
+
+            //spawn balls with various speeds (Left to Right)
+            if (randValue < 17)
+            {
+                    randValue = randGen.Next(10, 455 - meteorSize * 2);
+
+                    Rectangle ball = new Rectangle(0, randValue, meteorSize, meteorSize);
+                    meteorList.Add(ball);
+                    meteorSpeeds.Add(randGen.Next(5, 10));
+            }
+
+            //spawn balls with various speeds (Right to Left)
+            if (randValue > 20 && randValue < 34)
+            {
+                randValue = randGen.Next(10, 455 - meteorSize * 2);
+
+                Rectangle ball = new Rectangle(600, randValue, meteorSize, meteorSize);
+                meteorList.Add(ball);
+                meteorSpeeds.Add(randGen.Next(-10, -5));
+            }
+        }
+
+        public void MeteorHitsPlayer()
+        {
+            //Reset Player To Starting Position if Hit by a Meteor
+            for (int i = 0; i < meteorList.Count(); i++)
+            {
+                if (meteorList[i].IntersectsWith(player1))
+                {
+                    player1 = new Rectangle(182, 480, 10, 25);
+
+                    ExplosionSound.Play();
+                }
+                else if (meteorList[i].IntersectsWith(player2))
+                {
+                    player2 = new Rectangle(406, 480, 10, 25);
+
+                    ExplosionSound.Play();
+                }
+            }
+        }
+
+        public void ScoresOrWins()
+        {
+            //If Playr Scores Add a Point
+            if (player1.Y < 1)
+            {
+                player1.Y = 480;
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                PointSound.Play();
+            }
+
+            if (player2.Y < 1)
+            {
+                player2.Y = 480;
+                player2Score++;
+                p2ScoreLabel.Text = $"{player2Score}";
+
+                PointSound.Play();
+            }
+
+            //If Player Wins Display End Screen
+            if (player1Score == 3 || player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+
+                WinSound.Play();
+            }
+        }
+
+        public void ResetGame()
+        {
+            //Reset Everything To Allow Restart
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+
+            gameTimer.Enabled = true;
+
+            player1Score = 0;
+            player2Score = 0;
+
+            meteorList.Clear();
+            meteorSpeeds.Clear();
+
+            Rectangle player1 = new Rectangle(182, 480, 10, 25);
+            Rectangle player2 = new Rectangle(406, 480, 10, 25);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameTimer.Enabled == false && player1Score == 0 && player2Score == 0)
+            {
+                titleLabel.Text = "Space Race!";
+                subtitleLabel.Text = "Press Space to Start or Esc to End";
+            }
+            else if (gameTimer.Enabled == true)
+            {
+                //draw balls
+                for (int i = 0; i < meteorList.Count(); i++)
+                {
+                    e.Graphics.FillEllipse(whiteBrush, meteorList[i]);
+                }
+
+                //Draw All Other Objects
+                e.Graphics.FillRectangle(grayBrush, zoneborder1);
+                e.Graphics.FillRectangle(grayBrush, zoneborder2);
+                e.Graphics.FillRectangle(p1Brush, player1);
+                e.Graphics.FillRectangle(p2Brush, player2);
+            }
+            else
+            {
+                if (player1Score == 3)
+                {
+                    titleLabel.Text = $"P1 Wins!!!";
+                    subtitleLabel.Text = "Press Sace to Start and Escape to End";
+                }
+
+                if (player2Score == 3)
+                {
+                    titleLabel.Text = $"P2 Wins!!!";
+                    subtitleLabel.Text = "Press Sace to Start and Escape to End";
+                }
+            }
+        }
+    }
+}
